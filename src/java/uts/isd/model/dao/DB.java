26588741beeb831db71e94e1720aa8fb408c552a
Vincent <vakena@outlook.com>
2020-06-05 11:26:42 +1000

    database update, some CRUD functionality working - needs debugging

--- src/java/uts/isd/model/dao/DB.java
+++ src/java/uts/isd/model/dao/DB.java
@@ -8,9 +8,9 @@ import java.sql.Connection;
  */
 public abstract class DB {
 
-    protected String URL = "jdbc:derby://localhost:1527/customersdb [iotcustomer on IOTCUSTOMER]";//replace this string with your jdbc:derby local host url   
-    protected String db = "customersdb";//name of the database   
-    protected String dbuser = "iotcustomer";//db root user   
+    protected String URL = "jdbc:derby://localhost:1527/iotdb";//replace this string with your jdbc:derby local host url   
+    protected String db = "iotdb";//name of the database   
+    protected String dbuser = "administrator";//db root user   
     protected String dbpass = "password"; //db root password   
     protected String driver = "org.apache.derby.jdbc.ClientDriver"; //jdbc client driver - built in with NetBeans   
     protected Connection conn; //connection null-instance to be initialized in sub-classes
