f578a73080b10865ef44a57de9c2a10ff80fd17f
CROSBY <ncrosby17@ss-air16-1223.lan>
2020-06-08 16:52:28 +1000

    Added all elements to User.java

--- src/java/uts/isd/model/User.java
+++ src/java/uts/isd/model/User.java
@@ -1,14 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uts.isd.model;
 
-/**
- *
- * @author ncrosby17
- */
 public class User {
-    
+
+    //Registration data fields
+    private String fname;
+    private String lname;
+    private String password;
+    private String dob;
+    private String email;
+    private String address;
+    private String phone;
+    private String id;
+
+    //Initialise constructor with user fields
+    public User(String email, String fname, String lname, String password, String dob, String phone) {
+        this.email = email;
+        this.fname = fname;
+        this.lname = lname;
+        this.password = password;
+        this.dob = dob;
+        this.phone = phone;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
