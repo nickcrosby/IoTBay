62bf51e4d2fd6d519323424d0ff1526e7375f1c2
unknown <karlsilis97@gmail.com>
2020-06-08 16:30:08 +1000

    Products Table

---
+++ src/java/uts/isd/model/dao/DB.java
@@ -0,0 +1,17 @@
+package uts.isd.model.dao;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Karl Silis
+ */
+public abstract class DB {
+    //make sure to changes this to the right Database <3 
+    protected String URL = "jdbc:derby://localhost:1527/";
+    protected String db = "IoTBaydb";
+    protected String dbuser = "IoTUser";
+    protected String dbpass = "admin";
+    protected String driver = "org.apache.derby.jdbc.ClientDriver";
+    protected Connection conn;
+}
