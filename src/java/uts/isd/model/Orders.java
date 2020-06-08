fe3522d0aff7d8ee5dfb48c82afc8b3d8b46f09a
eduardomoraza <moraza.eduardo@gmail.com>
2020-06-09 01:22:25 +1000

    views

---
+++ src/java/uts/isd/model/Orders.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.model;
+
+/**
+ *
+ * @author eduardomoraza
+ */
+public class Orders {
+    private String orderID;
+    private String orderStatus;
+    private String totalCost;
+    private String productID;
+    private String quantity;
+    private String date;
+
+    public Orders(String orderID, String orderStatus, String totalCost, String productID, String quantity, String date) {
+        this.orderID = orderID;
+        this.orderStatus = orderStatus;
+        this.totalCost = totalCost;
+        this.productID = productID;
+        this.quantity = quantity;
+        this.date = date;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(String totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+  
+    
+
+}
